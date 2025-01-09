22b30df26182d96161f5a25ec4223de9ce25fbb6
davieongora <davidongora@gmail.com>
2025-01-09 23:56:44 +0300

    feat: application colors and themes

---
+++ lib/shared/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textColor = Color.fromARGB(255, 243, 239, 239);
+  static const Color linktext = Colors.blue;
+
+  static const Color whiteextColor = Color(0xff212121);
+
+  static const Color iconColor = Color(0xFF007438);
+  static const Color theme = Color(0xff007438);
+
+  static Color disabled = Colors.green[200] ?? Colors.green;
+  // static Color disabled = Colors.green[200] as Color;
+  static const Color enabled = Color(0xFF007438);
+  static const Color buttColor = Colors.white;
+
+  static const Color appbar = Colors.white;
+  static Color topbar = Colors.green[200] ?? Colors.green;
+
+  static const Color progress = Colors.orange;
+  static const Color error = Colors.red;
+  static const Color info = Colors.blue;
+  static const Color success = Colors.green;
+}
